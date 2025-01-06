6fd3930640a6924f3835053245249809df833bea
mimmoo-4 <175538898+mimmoo-4@users.noreply.github.com>
2025-01-06 22:41:53 +0000

    ファビコンルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :members
   resources :posts
   resources :genres
+  #favicon.icoへのリクエストを無視
+  get '/favicon.ico', to: ->(env) { [404, {}, []] }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
