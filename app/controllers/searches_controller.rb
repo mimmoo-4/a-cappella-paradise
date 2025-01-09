1859bc2961cbeb1d8e8ccfec0162593627387493
mimmoo-4 <175538898+mimmoo-4@users.noreply.github.com>
2025-01-09 05:34:00 +0000

    Add view

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,9 @@
 class SearchesController < ApplicationController
   before_action :authenticate_member!
-  
+
   def search
     @range = params[:range]
-    
+
     if @range == "Member"
       @members = Member.looks(params[:search], params[:word])
     else
