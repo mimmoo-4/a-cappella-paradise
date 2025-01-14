1c53efe483f4b087a9553132e94d52dad8a740ed
mimmoo-4 <175538898+mimmoo-4@users.noreply.github.com>
2025-01-14 14:40:05 +0000

    finish migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_14_120219) do
+ActiveRecord::Schema.define(version: 2025_01_14_143701) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2025_01_14_120219) do
     t.string "main_part"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
