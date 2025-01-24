52919f0dd967f89518170e8b33efc123c979a552
mimmoo-4 <175538898+mimmoo-4@users.noreply.github.com>
2025-01-24 08:01:38 +0000

    [Finish]add-admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,19 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_member!, except: [:top], unless: :admin_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-  def configure_authentication
-    if admin_controller?
-      authenticate_admin!
-    else
-      authenticate_member! unless action_is_public?
-    end
+  #サインイン後にマイページに遷移
+  def after_sign_in_path_for(resource)
+    member_path(current_member.id)
   end
 
+  private
   def admin_controller?
     self.class.module_parent_name == 'Admin'
   end
 
-  def action_is_public?
-    controller_name == 'homes' && action_name == 'top'
-  end
+  protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:nickname,:address,:furigana,:self_introduction,:main_part])
+  end
 end
