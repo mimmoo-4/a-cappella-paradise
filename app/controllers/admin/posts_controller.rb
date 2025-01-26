fa494e2ae3b2ac04558a08f04bd606a4d3e9cee4
mimmoo-4 <175538898+mimmoo-4@users.noreply.github.com>
2025-01-26 01:49:21 +0000

    投稿詳細-修正

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -12,4 +12,12 @@ class Admin::PostsController < ApplicationController
     redirect_to admin_dashboards_path, notice: '投稿を削除しました。'
   end
 
+  def show
+    @post = Post.find(params[:id])
+    @post_comment = PostComment.new
+    @genre = Genre.find(@post.genre_id)
+    @member = Member.find(@post.member_id)
+  end
+
+
 end
